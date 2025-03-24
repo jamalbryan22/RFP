69542c2bc205070263617dcaaaa7f515676c76b2
jamalbryan22 <jamal_bryan@knights.ucf.edu>
2025-03-24 03:00:20 -0400

    add seed data

--- RFP_App.Server/Migrations/20250324064250_ExpandedSeedData.Designer.cs
+++ RFP_App.Server/Migrations/20250324064250_ExpandedSeedData.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RFP_APP.Server.Data;
 
@@ -10,9 +11,11 @@ using RFP_APP.Server.Data;
 namespace RFP_APP.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250324064250_ExpandedSeedData")]
+    partial class ExpandedSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
