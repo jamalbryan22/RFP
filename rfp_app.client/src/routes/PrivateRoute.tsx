5c05a5ac6645d99e1c0ee27e4c72d18b25d5c18e
Jamal Bryan <42558543+jamalbryan22@users.noreply.github.com>
2025-04-20 05:42:40 +0000

    frontend update

--- rfp_app.client/src/routes/PrivateRoute.tsx
+++ rfp_app.client/src/routes/PrivateRoute.tsx
@@ -1,3 +1,4 @@
+import { JSX } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
@@ -5,3 +6,5 @@ const PrivateRoute = ({ children }: { children: JSX.Element }) => {
   const { token } = useAuth();
   return token ? children : <Navigate to="/login" replace />;
 };
+
+export default PrivateRoute;
